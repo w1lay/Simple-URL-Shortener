982e0eb3b8921632097c692f8796eeb5842ee143
andrey <andreysturbo@mail.ru>
2025-11-05 20:22:14 +0400

    Правильная работа с SQLX

--- internal/repositories/link_postgres.go
+++ internal/repositories/link_postgres.go
@@ -1,8 +1,6 @@
 package repositories
 
 import (
-	"fmt"
-
 	"github.com/jmoiron/sqlx"
 	"github.com/w1lay/Simple-URL-Shortener/internal/dtos"
 )
@@ -17,18 +15,13 @@ func NewLinkPostgresRepository(db *sqlx.DB) *LinkPostgresRepository {
 
 func (r *LinkPostgresRepository) SaveLinkRecord(dto dtos.Link) error {
 	// ToDO: сделать нормально с аргументами вида $1
-	query := fmt.Sprintf(
-		`INSERT INTO links (short_link, long_link) VALUES ('%s', '%s');`,
-		dto.ShortLink,
-		dto.LongLink,
-	)
-	fmt.Println(query)
-	_, err := r.db.Exec(query)
+	query := `INSERT INTO links (short_link, long_link) VALUES ($1, $2)`
+	_, err := r.db.Exec(query, dto.ShortLink, dto.LongLink)
 	return err
 }
 
 func (r *LinkPostgresRepository) FindLinkByShortLink(shortLink string) (*dtos.Link, error) {
-	query := `SELECT * FROM links WHERE short_link = '$1';`
+	query := `SELECT * FROM links WHERE short_link = $1`
 	var link dtos.Link
 
 	err := r.db.Get(&link, query, shortLink)
